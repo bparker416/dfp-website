a022aadefeae6588323cc576daf84de9e92b213b
bparker416 <90215187+bparker416@users.noreply.github.com>
2024-11-01 18:18:18 -0700

    Routing complete and footer begun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import {CommonModule} from "@angular/common";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, RouterLink, RouterLinkActive],
+  imports: [ RouterOutlet, HeaderComponent, FooterComponent, RouterLink, RouterLinkActive, CommonModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { AboutComponent } from "./about/about.component";
 import { MenuComponent } from "./menu/menu.component";
 import { DrinksComponent } from "./drinks/drinks.component";
 import { SocialEventsComponent } from "./social-events/social-events.component";
+import {HomeComponent} from "./home/home.component";
 
 
 
 export const routes: Routes = [
-  { path: '', component: MenuComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent},
   { path: 'menu', component: MenuComponent},
   { path: 'drinks', component: DrinksComponent},
