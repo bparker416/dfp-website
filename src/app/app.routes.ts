fd1b0ca23d0bc3d1876ee91db531106687e5cbe4
bparker416 <90215187+bparker416@users.noreply.github.com>
2024-11-01 19:59:22 -0700

    Routing updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AboutComponent } from "./about/about.component";
 import { MenuComponent } from "./menu/menu.component";
 import { DrinksComponent } from "./drinks/drinks.component";
 import { SocialEventsComponent } from "./social-events/social-events.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {HomeComponent} from "./home/home.component";
 
 
@@ -10,8 +11,9 @@ import {HomeComponent} from "./home/home.component";
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent},
-  { path: 'menu', component: MenuComponent},
-  { path: 'drinks', component: DrinksComponent},
-  { path: 'social-events', component: SocialEventsComponent},
+  { path: 'about', component: AboutComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'drinks', component: DrinksComponent },
+  { path: 'social-events', component: SocialEventsComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
