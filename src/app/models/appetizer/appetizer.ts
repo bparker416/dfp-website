1422fe404aa75ed77307bdc2957eb90c3e12688b
bparker416 <90215187+bparker416@users.noreply.github.com>
2024-11-21 23:47:10 -0800

    Create models and services for menu component

---
+++ src/app/models/appetizer/appetizer.ts
@@ -0,0 +1,7 @@
+export interface Appetizer {
+  app_id: number;
+  app_name: string;
+  app_description: string;
+  app_price: number;
+  additional_text: string;
+}
---
+++ src/app/models/pizza/pizza.ts
@@ -0,0 +1,8 @@
+export interface PizzaModule {
+  pizza_id: number;
+  pizza_name: string;
+  pizza_description: string;
+  small_price: number;
+  large_price: number;
+  addition_text: string;
+}
