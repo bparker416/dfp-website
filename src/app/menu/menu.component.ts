9d6bcbda7129f844ea0cc6c8b72cb0761f444e99
bparker416 <90215187+bparker416@users.noreply.github.com>
2024-11-21 19:46:28 -0800

    Menu backend integration begun

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import {PizzaListComponent} from "../pizza-list/pizza-list.component";
+import {HttpClientModule} from "@angular/common/http";
+import {PizzaService} from "../models/pizza/pizza.service";
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [ HeaderComponent ],
+  imports: [HeaderComponent, PizzaListComponent, HttpClientModule],
+  providers: [PizzaService],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
-
+  constructor(private pizzaService: PizzaService) {}
 }
