f6e142990ac52bbe78eedc753e0492aee1766d09
bparker416 <90215187+bparker416@users.noreply.github.com>
2024-11-26 16:23:32 -0800

    Create models for food-menu

---
+++ src/app/meat/meat.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {MeatService} from "../models/meat/meat.service";
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-meat',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './meat.component.html',
+  styleUrl: './meat.component.css'
+})
+export class MeatComponent implements OnInit{
+  meatPriceByThree: any[] = [];
+  meatPriceByFour: any[] = [];
+
+  constructor(private meatService: MeatService) { }
+
+  ngOnInit(): void {
+    this.meatService.meatPriceByThree().subscribe( (data) => { this.meatPriceByThree = data });
+    this.meatService.meatPriceByFour().subscribe( (data) => { this.meatPriceByFour = data });
+  }
+}
