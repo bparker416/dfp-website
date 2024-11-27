7d8a7771e32203e9d767888b6e5011fe0efb35a9
bparker416 <90215187+bparker416@users.noreply.github.com>
2024-11-26 16:45:23 -0800

    Fix veggie.service.ts bug

--- src/app/models/veggie/veggie.service.ts
+++ src/app/models/veggie/veggie.service.ts
@@ -11,7 +11,7 @@ export class VeggieService {
 
   constructor(private http: HttpClient) { }
 
-  getVeggies(): Observable<any[]> {
+  getVeggies(): Observable<any> {
     return this.http.get(this.apiUrl);
   }
 }
