31f7964f8d8cf82422eb72a22e39419c7bcbfc6b
bparker416 <90215187+bparker416@users.noreply.github.com>
2024-11-26 17:41:19 -0800

    food-menu components tested to ensure services work correctly

--- src/app/food-menu/menu/menu.component.ts
+++ src/app/food-menu/menu/menu.component.ts
@@ -7,15 +7,25 @@ import {AppetizerService} from "../../models/food-menu-models/appetizer/appetize
 import {AppetizerComponent} from "../appetizer/appetizer.component";
 import {SideComponent} from "../side/side.component";
 import {SideService} from "../../models/food-menu-models/side/side.service";
+import {SaladComponent} from "../salad/salad.component";
+import {SaladService} from "../../models/food-menu-models/salad/salad.service";
+import {SandoService} from "../../models/food-menu-models/sando/sando.service";
+import {SandoComponent} from "../sando/sando.component";
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [HeaderComponent, PizzaListComponent, AppetizerComponent, HttpClientModule, SideComponent],
-  providers: [PizzaService, AppetizerService, SideService],
+  imports: [HeaderComponent, PizzaListComponent, AppetizerComponent, HttpClientModule, SideComponent, SaladComponent, SandoComponent],
+  providers: [PizzaService, AppetizerService, SideService, SaladService, SandoService],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
-  constructor(private pizzaService: PizzaService, private appetizerService: AppetizerService, private sideService: SideService) {}
+  constructor(
+    private pizzaService: PizzaService,
+    private appetizerService: AppetizerService,
+    private sideService: SideService,
+    private saladService: SaladService,
+    private sandoService: SandoService,
+  ) {}
 }
