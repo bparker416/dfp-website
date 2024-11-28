3f9a72ac8b56db0f39950de5ecf63bfc52dd98fd
bparker416 <90215187+bparker416@users.noreply.github.com>
2024-11-27 20:53:14 -0800

    veggie.component.html complete

--- src/app/food-menu/byo/byo.component.ts
+++ src/app/food-menu/byo/byo.component.ts
@@ -5,12 +5,14 @@ import {HttpClientModule} from "@angular/common/http";
 import {SauceService} from "../../models/food-menu-models/sauce/sauce.service";
 import {CheeseService} from "../../models/food-menu-models/cheese/cheese.service";
 import {CheeseComponent} from "../cheese/cheese.component";
+import {VeggieComponent} from "../veggie/veggie.component";
+import {VeggieService} from "../../models/food-menu-models/veggie/veggie.service";
 
 @Component({
   selector: 'app-byo',
   standalone: true,
-  imports: [CommonModule, SauceComponent, HttpClientModule, CheeseComponent],
-  providers: [HttpClientModule, SauceService, CheeseService],
+  imports: [CommonModule, SauceComponent, HttpClientModule, CheeseComponent, VeggieComponent],
+  providers: [HttpClientModule, SauceService, CheeseService, VeggieService],
   templateUrl: './byo.component.html',
   styleUrl: './byo.component.css'
 })
