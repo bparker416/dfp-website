c322250e621c133f46dea284b0b591d14c0ccb81
bparker416 <90215187+bparker416@users.noreply.github.com>
2024-11-29 14:25:26 -0800

    sauce.component.html, cheese.component.html, veggie.component.html, meat.component.html lists retrieved and displayed

--- src/app/food-menu/byo/byo.component.ts
+++ src/app/food-menu/byo/byo.component.ts
@@ -7,12 +7,14 @@ import {CheeseService} from "../../models/food-menu-models/cheese/cheese.service
 import {CheeseComponent} from "../cheese/cheese.component";
 import {VeggieComponent} from "../veggie/veggie.component";
 import {VeggieService} from "../../models/food-menu-models/veggie/veggie.service";
+import {MeatComponent} from "../meat/meat.component";
+import {MeatService} from "../../models/food-menu-models/meat/meat.service";
 
 @Component({
   selector: 'app-byo',
   standalone: true,
-  imports: [CommonModule, SauceComponent, HttpClientModule, CheeseComponent, VeggieComponent],
-  providers: [HttpClientModule, SauceService, CheeseService, VeggieService],
+  imports: [CommonModule, SauceComponent, HttpClientModule, CheeseComponent, VeggieComponent, MeatComponent],
+  providers: [HttpClientModule, SauceService, CheeseService, VeggieService, MeatService],
   templateUrl: './byo.component.html',
   styleUrl: './byo.component.css'
 })
