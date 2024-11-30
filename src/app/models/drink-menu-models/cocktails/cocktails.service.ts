c42e8a88e6344addaf9af28555577fdc1cd9f678
bparker416 <90215187+bparker416@users.noreply.github.com>
2024-11-30 15:37:16 -0800

    drink-menu service complete

--- src/app/models/drink-menu-models/cocktails/cocktails.service.ts
+++ src/app/models/drink-menu-models/cocktails/cocktails.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CocktailsService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/api/house-cocktails';
+
+  constructor(private http: HttpClient) { }
+
+  getCocktails(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
 }
--- src/app/models/drink-menu-models/libations/libations.service.ts
+++ src/app/models/drink-menu-models/libations/libations.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LibationsService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/api/seasonal-libations';
+
+  constructor(private http: HttpClient) { }
+
+  getLibations(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
 }
