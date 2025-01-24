9d0971ec3865493a4a5ab81a03e7e0b7b16a6970
bparker416 <90215187+bparker416@users.noreply.github.com>
2025-01-23 17:24:24 -0800

    Push to azure static

--- dist/dfp-website/server/main.server.mjs
+++ dist/dfp-website/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-6FAY6KVG.mjs";import"./chunk-37QNZTTG.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-7C4DUWQF.mjs";import"./chunk-37QNZTTG.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
