ab7b601eb39f75e37590d5e90cdec362a0b6c549
bparker416 <90215187+bparker416@users.noreply.github.com>
2025-01-29 00:42:43 -0800

    Update Dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import {provideHttpClient} from "@angular/common/http";
+import {provideServerRendering} from "@angular/platform-server";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient()]
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), provideServerRendering()]
 };
